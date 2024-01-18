ee97ad2e325bf113fb3f95eac44f18fd2bdd5112
taskingaijc <jiang.c@wanmoai.com>
2024-01-18 16:31:35 +0800

    test:  fix test data for create record and update record

--- backend/tests/services_tests/retrieval/test_record.py
+++ backend/tests/services_tests/retrieval/test_record.py
@@ -16,8 +16,28 @@ class TestRecord(Retrieval):
 
         create_record_data = {
             "type": "text",
-            "title": "test create record",
-            "content": "This is a test for create record.",
+            "title": "TaskingAI Introduction",
+            "content":
+                    """Introduction
+                       TaskingAI is an AI-native application development platform that unifies modules like Model, Retrieval, Assistant, and Tool into one seamless ecosystem, streamlining the creation and deployment of applications for developers.
+                        
+                        Key Concepts
+                        Project
+                        Projects in TaskingAI are organizational units designed to group related activities and resources. They offer a structured way to manage different initiatives or brands, allowing for clear segregation and management. Each project can be tailored with specific settings and resources, ensuring that the information and activities within one project remain distinct and isolated from others.
+                        
+                        Model
+                        TaskingAI incorporates a variety of chat completion models, each with distinct capabilities and attributes. These models serve as the core 'brains' of AI assistants, providing them with reasoning and logical capabilities. TaskingAI supports models from multiple providers, each offering different strengths in terms of input token limits, reasoning, and logic capabilities. Users can select and switch between models based on their specific needs and the complexity of the tasks at hand.
+                        
+                        Retrieval
+                        Retrievals in TaskingAI are mechanisms that enable AI assistants to access and utilize external knowledge bases. This feature allows the integration of additional information into the AI's responses, enhancing its ability to provide accurate and context-relevant answers. Retrievals are crucial for tasks that require specific, detailed, or up-to-date information, ensuring that the AI's responses are not limited by its pre-training data.
+                        
+                        Assistant
+                        The Assistant feature in TaskingAI refers to the AI entities capable of performing a wide range of tasks. These assistants are customizable and can be tailored to suit various applications, from customer service to internal training. They operate based on the models and tools provided, and their functionality can be extended through the use of retrievals, allowing them to access a broader range of information and capabilities.
+                        
+                        Tool
+                        Tools in TaskingAI are functionalities that enable AI assistants to interact with external resources and perform specific actions, such as fetching live information or communicating with external systems. These tools are typically defined in OpenAPI schema format and can be attached to assistants to enhance their capabilities. Tools are essential for tasks that require real-time data or interaction with external APIs and services, making the assistants more dynamic and versatile in their operation.
+                        
+                        TaskingAI is more than just a platform; it's a gateway to unlocking the full potential of AI in your daily tasks. Whether you're a developer, a researcher, or someone looking to streamline their workflow, TaskingAI offers the tools and resources to achieve your goals.""",
             "text_splitter": {
                 "type": "token",
                 "chunk_size": 200,
@@ -76,17 +96,63 @@ class TestRecord(Retrieval):
     @pytest.mark.asyncio
     async def test_update_record(self):
         update_record_data = {
-            "type": "text",
-            "title": "test update record",
-            "content": "This is a test for update record.",
-            "text_splitter": {
+             "type": "text",
+             "title": "TaskingAI Introduction",
+             "content":
+                     """Introduction
+                       TaskingAI is an AI-native application development platform that unifies modules like Model, 
+                       Retrieval, Assistant, and Tool into one seamless ecosystem, streamlining the creation and 
+                       deployment of applications for developers.
+                        
+                        Key Concepts
+                        Project
+                        Projects in TaskingAI are organizational units designed to group related activities and 
+                        resources. They offer a structured way to manage different initiatives or brands, allowing for 
+                        clear segregation and management. Each project can be tailored with specific settings and 
+                        resources, ensuring that the information and activities within one project remain distinct and 
+                        isolated from others.
+                        
+                        Model
+                        TaskingAI incorporates a variety of chat completion models, each with distinct capabilities and 
+                        attributes. These models serve as the core 'brains' of AI assistants, providing them with 
+                        reasoning and logical capabilities. TaskingAI supports models from multiple providers, each 
+                        offering different strengths in terms of input token limits, reasoning, and logic capabilities. 
+                        Users can select and switch between models based on their specific needs and the complexity of 
+                        the tasks at hand.
+                        
+                        Retrieval
+                        Retrievals in TaskingAI are mechanisms that enable AI assistants to access and utilize external 
+                        knowledge bases. This feature allows the integration of additional information into the AI's 
+                        responses, enhancing its ability to provide accurate and context-relevant answers. Retrievals 
+                        are crucial for tasks that require specific, detailed, or up-to-date information, ensuring that 
+                        the AI's responses are not limited by its pre-training data.
+                        
+                        Assistant
+                        The Assistant feature in TaskingAI refers to the AI entities capable of performing a wide range 
+                        of tasks. These assistants are customizable and can be tailored to suit various applications, 
+                        from customer service to internal training. They operate based on the models and tools provided, 
+                        and their functionality can be extended through the use of retrievals, allowing them to access 
+                        a broader range of information and capabilities.
+                        
+                        Tool
+                        Tools in TaskingAI are functionalities that enable AI assistants to interact with external 
+                        resources and perform specific actions, such as fetching live information or communicating with 
+                        external systems. These tools are typically defined in OpenAPI schema format and can be attached 
+                        to assistants to enhance their capabilities. Tools are essential for tasks that require 
+                        real-time data or interaction with external APIs and services, making the assistants more 
+                        dynamic and versatile in their operation.
+                        
+                        TaskingAI is more than just a platform; it's a gateway to unlocking the full potential of AI in 
+                        your daily tasks. Whether you're a developer, a researcher, or someone looking to streamline 
+                        their workflow, TaskingAI offers the tools and resources to achieve your goals.""",
+             "text_splitter": {
                 "type": "token",
-                "chunk_size": 500,
-                "chunk_overlap": 200
-            },
-            "metadata": {
-                "key": "value"
-            }
+                "chunk_size": 200,
+                "chunk_overlap": 100
+             },
+             "metadata": {
+                "key1": "value1"
+             }
         }
         res = await update_record(Retrieval.collection_id, Retrieval.record_id, update_record_data)
         res_json = res.json()
