1d9f3ee30f37c7e2b98021246b74731d383f570d
jameszyao <thehuiger@gmail.com>
2024-01-22 21:18:55 +0800

    test: update list provider testcase

--- backend/tests/services_tests/model/test_provider.py
+++ backend/tests/services_tests/model/test_provider.py
@@ -4,7 +4,6 @@ from tests.services_api.model.provider import list_providers, get_provider
 
 
 class TestProvider:
-
     provider_list = ["object", "provider_id", "name", "credentials_schema"]
     provider_keys = set(provider_list)
     provider_credentials_schema_list = ["type", "properties", "required", "additionalProperties"]
@@ -13,20 +12,19 @@ class TestProvider:
     @pytest.mark.asyncio
     @pytest.mark.run(order=21)
     async def test_list_providers(self):
-
         res = await list_providers()
         res_json = res.json()
         assert res.status_code == 200
         assert res_json.get("status") == "success"
-        assert len(res_json.get("data")) == 5
-        assert res_json.get("fetched_count") == 5
-        assert res_json.get("total_count") == 5
+        assert len(res_json.get("data")) > 0
+        total = len(res_json.get("data"))
+        assert res_json.get("fetched_count") == total
+        assert res_json.get("total_count") == total
         assert res_json.get("has_more") is False
 
     @pytest.mark.asyncio
     @pytest.mark.run(order=22)
     async def test_get_provider(self):
-
         params = {"provider_id": "openai"}
         res = await get_provider(params)
         res_json = res.json()
