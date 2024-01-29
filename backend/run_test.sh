4b2febb0572e61a46cad738131425b27c400f9fb
jameszyao <thehuiger@gmail.com>
2024-01-30 00:33:47 +0800

    chore: update server version

--- backend/run_test.sh
+++ backend/run_test.sh
@@ -1,6 +1,5 @@
 # mode
-export MODE=dev
-#
+export MODE=test
 export PURPOSE=WEB
 
 # service
@@ -17,14 +16,15 @@ export HTTP_PROXY_URL=http://127.0.0.1:7890
 export TASKINGAI_INFERENCE_URL=http://127.0.0.1:8040
 
 # secret
-export AES_ENCRYPTION_KEY=aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-export JWT_SECRET_KEY=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+export AES_ENCRYPTION_KEY=7700b2f9c8dd982dfaddf8b47a92f1d900507ee8ac335f96a64e9ca0f018b195
+export JWT_SECRET_KEY=020b7c80bfb2a944a19ffa054ee96039117ec7066b7b10263ac3ac22b4c985a0
 export DEFAULT_ADMIN_USERNAME=admin
 export DEFAULT_ADMIN_PASSWORD=passWord123
 
 # log
 export LOG_LEVEL=INFO
 
+# test web service
 set -e
 gunicorn --bind 0.0.0.0:8010 \
          --preload \
@@ -38,6 +38,7 @@ current_dir=$(pwd)
 export PYTHONPATH=$PYTHONPATH:$current_dir
 pytest tests/services_tests/
 
+# test api service
 export PURPOSE=API
 export SERVICE_PORT=8020
 export API_SERVICE_PORT=8020
@@ -53,7 +54,3 @@ pytest tests/services_tests/assistant/
 pytest tests/services_tests/inference/
 pytest tests/services_tests/retrieval/
 pytest tests/services_tests/tool/
-
-
-
-
