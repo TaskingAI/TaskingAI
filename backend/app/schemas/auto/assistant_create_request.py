843e9771cd68d3fd6edd8ac3cb94ac5bf6cd7f25
jameszyao <thehuiger@gmail.com>
2024-03-07 00:33:26 +0800

    feat: update API schemas

--- backend/app/schemas/auto/assistant_create_request.py
+++ backend/app/schemas/auto/assistant_create_request.py
@@ -12,7 +12,8 @@ License: Apache 2.0
 """
 
 from pydantic import BaseModel, Field
-from typing import Dict
+from typing import Dict, List
+from app.models import *
 
 __all__ = ["AssistantCreateRequest"]
 
@@ -21,5 +22,9 @@ class AssistantCreateRequest(BaseModel):
     model_id: str = Field(..., min_length=8, max_length=8, pattern="^[a-zA-Z0-9]+$")
     name: str = Field("", min_length=0, max_length=127)
     description: str = Field("", min_length=0, max_length=512)
+    system_prompt_template: List[str] = Field([])
+    memory: AssistantMemory = Field(...)
+    tools: List[ToolRef] = Field([])
+    retrievals: List[RetrievalRef] = Field([])
+    retrieval_configs: RetrievalConfig = Field(...)
     metadata: Dict = Field({}, min_length=0, max_length=16)
-    max_count: int = Field(0, ge=0)
