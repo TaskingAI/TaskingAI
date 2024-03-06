196f27784f1b871ecf61fc3092ca931fbd5846d9
jameszyao <thehuiger@gmail.com>
2024-03-06 20:18:40 +0800

    refactor: replace model code

--- backend/common/models/assistant/memory/__init__.py
+++
@@ -1,5 +0,0 @@
-from ._build import build_chat_memory, build_assistant_memory
-from ._base import ChatMemory, AssistantMemory, MemoryType
-from .zero import AssistantZeroMemory
-from .naive import AssistantNaiveMemory
-from .message_window import AssistantMessageWindowMemory
--- backend/common/models/retrieval/text_splitter/__init__.py
+++
@@ -1,3 +0,0 @@
-from ._base import TextSplitter, TextSplitterType
-from .token import TokenTextSplitter
-from ._build import build_text_splitter
