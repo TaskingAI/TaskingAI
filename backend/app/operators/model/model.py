14beced2e9d5be117562a494c4c60fb284ef270e
jameszyao <thehuiger@gmail.com>
2024-04-05 22:10:18 +0800

    feat: allow model config update

--- backend/app/operators/model/model.py
+++ backend/app/operators/model/model.py
@@ -1,8 +1,8 @@
 from tkhelper.models import RedisOperator, ModelEntity
 from tkhelper.models.operator.postgres_operator import PostgresModelOperator
-from tkhelper.error import raise_request_validation_error
+from tkhelper.error import raise_http_error, raise_request_validation_error, ErrorCode
 from tkhelper.utils import check_http_error
-from typing import Dict, Tuple, Optional
+from typing import Dict, Tuple
 
 from app.database import redis_conn, postgres_pool
 from app.models import Model, ModelSchema, Provider, ModelType
@@ -43,19 +43,23 @@ def _build_display_credentials(original_credentials: Dict, credential_schema: Di
 
 async def verify_model_credentials(
     model_schema_id: str,
-    provider_model_id: Optional[str],
-    model_type: Optional[str],
+    provider_model_id: str,
+    properties: Dict,
+    model_type: str,
     credentials: Dict,
-    properties: Optional[Dict],
-) -> Tuple[str, str, str, Dict, Dict, Dict]:
+    encrypted_credentials: Dict = None,
+) -> Tuple[str, str, str, str, Dict, Dict, Dict]:
     """
-    Verify model credentials
+    verify model credentials and build display credentials
     :param model_schema_id: the model schema id
     :param provider_model_id: the provider model id
+    :param properties: the properties
     :param model_type: the model type
     :param credentials: the credentials
-    :param properties: the properties
-    :return: a tuple of provider_id, provider_model_id, model_type, properties, encrypted_credentials, display_credentials
+    :param encrypted_credentials: the encrypted credentials
+    :return: a tuple of (the model schema id, provider id, provider model id, model type,
+        encrypted credentials, display credentials, properties)
+
     """
     from app.services.model import get_model_schema, get_provider
     from app.services.inference import verify_credentials
@@ -63,11 +67,15 @@ async def verify_model_credentials(
     # verify model schema exists
     model_schema: ModelSchema = get_model_schema(model_schema_id)
     if not model_schema:
-        raise_request_validation_error(message=f"Model schema {model_schema_id} not found.")
+        raise_http_error(
+            error_code=ErrorCode.OBJECT_NOT_FOUND,
+            message=f"Model schema {model_schema_id} not found.",
+        )
 
     # get provider
     provider: Provider = get_provider(model_schema.provider_id)
-    provider_id = provider.provider_id
+
+    provider_model_id = provider_model_id or model_schema.provider_model_id
 
     properties = properties or model_schema.properties
     provider_model_id = provider_model_id or model_schema.provider_model_id
@@ -80,127 +88,124 @@ async def verify_model_credentials(
     else:
         model_type = model_schema.type
 
-        # verify model credentials
+    # verify model credentials
     response = await verify_credentials(
         model_schema_id=model_schema.model_schema_id,
         provider_model_id=provider_model_id,
         model_type=model_type,
         credentials=credentials,
+        encrypted_credentials=encrypted_credentials,
         properties=properties,
     )
     check_http_error(response)
     response_data = response.json()["data"]
     encrypted_credentials = response_data["encrypted_credentials"]
     properties = response_data["properties"]
-    display_credentials = _build_display_credentials(
-        original_credentials=credentials,
-        credential_schema=provider.credentials_schema,
+    display_credentials = None
+    if credentials:
+        display_credentials = _build_display_credentials(
+            original_credentials=credentials,
+            credential_schema=provider.credentials_schema,
+        )
+
+    return (
+        model_schema_id,
+        model_schema.provider_id,
+        provider_model_id,
+        model_type,
+        encrypted_credentials,
+        display_credentials,
+        properties,
     )
 
-    return (provider_id, provider_model_id, model_type, properties, encrypted_credentials, display_credentials)
 
-
-class ModelModelOperator(PostgresModelOperator):
+class ModelOperator(PostgresModelOperator):
     async def create(
         self,
         create_dict: Dict,
         **kwargs,
     ) -> ModelEntity:
-        # handle kwargs
-        self._check_kwargs(object_id_required=None, **kwargs)
-        model_schema_id = create_dict["model_schema_id"]
-        name = create_dict["name"]
-
         # verify model credentials
         (
+            model_schema_id,
             provider_id,
             provider_model_id,
             model_type,
-            properties,
             encrypted_credentials,
             display_credentials,
+            properties,
         ) = await verify_model_credentials(
-            model_schema_id=model_schema_id,
+            model_schema_id=create_dict["model_schema_id"],
             provider_model_id=create_dict.get("provider_model_id"),
+            properties=create_dict.get("properties"),
             model_type=create_dict.get("type"),
             credentials=create_dict["credentials"],
-            properties=create_dict.get("properties"),
         )
 
-        # create model
         model = await super().create(
             create_dict={
                 "model_schema_id": model_schema_id,
                 "provider_id": provider_id,
                 "provider_model_id": provider_model_id,
-                "name": name,
+                "name": create_dict["name"],
                 "type": model_type,
                 "encrypted_credentials": encrypted_credentials,
                 "display_credentials": display_credentials,
                 "properties": properties,
             },
-            **kwargs,
         )
-
         return model
 
-    async def update(
-        self,
-        update_dict: Dict,
-        **kwargs,
-    ) -> ModelEntity:
-        """
-        Update a model
-        :param update_dict: the model update dictionary
-        :param kwargs: other parameters
-        :return: the updated model
-        """
+    async def update(self, update_dict: Dict, **kwargs) -> ModelEntity:
+        model_id = kwargs["model_id"]
+        model: Model = await super().get(model_id=model_id)
 
-        self._check_kwargs(object_id_required=True, **kwargs)
-        model_id = kwargs.get("model_id")
-
-        name = update_dict.get("name")
-        credentials = update_dict.get("credentials")
-
-        # get model
-        model = await self.get(model_id=model_id)
-
-        # prepare update dict
         new_update_dict = {}
+        if update_dict.get("name") is not None:
+            new_update_dict["name"] = update_dict.get("name")
 
-        if name is not None:
-            new_update_dict["name"] = name
+        model_schema_id = update_dict.get("model_schema_id")
+        provider_model_id = update_dict.get("provider_model_id")
+        model_type = update_dict.get("type")
+        credentials = update_dict.get("credentials")
+        properties = update_dict.get("properties")
 
-        if credentials is not None:
+        if model_schema_id or provider_model_id or model_type or credentials or properties:
             # verify model credentials
             (
-                provider_id,
-                provider_model_id,
-                model_type,
-                properties,
-                encrypted_credentials,
-                display_credentials,
+                new_model_schema_id,
+                new_provider_id,
+                new_provider_model_id,
+                new_model_type,
+                new_encrypted_credentials,
+                new_display_credentials,
+                new_properties,
             ) = await verify_model_credentials(
-                model_schema_id=update_dict.get("model_schema_id") or model.model_schema_id,
-                provider_model_id=update_dict.get("provider_model_id") or model.provider_model_id,
-                model_type=update_dict.get("type") or model.type,
-                credentials=update_dict.get("credentials") or model.credentials,
-                properties=update_dict.get("properties") or model.properties,
+                model_schema_id=model_schema_id or model.model_schema_id,
+                provider_model_id=provider_model_id or model.provider_model_id,
+                properties=properties or model.properties,
+                model_type=model_type or model.type,
+                credentials=credentials,
+                encrypted_credentials=model.encrypted_credentials if not credentials else None,
             )
-            new_update_dict["provider_id"] = provider_id
-            new_update_dict["provider_model_id"] = provider_model_id
-            new_update_dict["type"] = model_type
-            new_update_dict["properties"] = properties
-            new_update_dict["encrypted_credentials"] = encrypted_credentials
-            new_update_dict["display_credentials"] = display_credentials
 
-        return await super().update(
+            new_update_dict["model_schema_id"] = new_model_schema_id
+            new_update_dict["provider_id"] = new_provider_id
+            new_update_dict["provider_model_id"] = new_provider_model_id
+            new_update_dict["type"] = new_model_type
+            new_update_dict["properties"] = new_properties
+            if credentials:
+                new_update_dict["encrypted_credentials"] = new_encrypted_credentials
+                new_update_dict["display_credentials"] = new_display_credentials
+
+        model = await super().update(
+            model_id=model_id,
             update_dict=new_update_dict,
-            **kwargs,
         )
+        return model
 
 
-model_ops = ModelModelOperator(
+model_ops = ModelOperator(
     postgres_pool=postgres_pool,
     entity_class=Model,
     redis=RedisOperator(
--- backend/app/services/inference/verify.py
+++ backend/app/services/inference/verify.py
@@ -13,19 +13,25 @@ async def verify_credentials(
     model_schema_id: str,
     provider_model_id: Optional[str],
     model_type: Optional[str],
-    credentials: Dict,
+    credentials: Optional[Dict],
+    encrypted_credentials: Optional[Dict],
     properties: Optional[Dict],
 ) -> ResponseWrapper:
+    payload = {
+        "model_schema_id": model_schema_id,
+        "provider_model_id": provider_model_id,
+        "model_type": model_type,
+        "properties": properties,
+    }
+    if credentials is not None:
+        payload["credentials"] = credentials
+    else:
+        payload["encrypted_credentials"] = encrypted_credentials
+
     async with aiohttp.ClientSession() as session:
         response = await session.post(
             f"{CONFIG.TASKINGAI_INFERENCE_URL}/v1/verify_credentials",
-            json={
-                "model_schema_id": model_schema_id,
-                "provider_model_id": provider_model_id,
-                "model_type": model_type,
-                "credentials": credentials,
-                "properties": properties,
-            },
+            json=payload,
         )
         return ResponseWrapper(response.status, await response.json())
 
