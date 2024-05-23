fc35f4f94af71676713eb95da1101341ad7ee02a
taskingaiwww <148739496+taskingaiwww@users.noreply.github.com>
2024-05-23 17:14:59 +0800

    feat: add dashboard module

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,4 +8,14 @@ export default defineConfig({
       '@': '/src',
     }
   },
+
+  server: {
+    proxy: {
+      "/api": {
+        target:"http://192.168.3.31:8080",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
 })
