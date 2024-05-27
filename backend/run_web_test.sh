1fa1abde8dc9b82b1b27f9ae070797fef0c3b701
taskingaijc <150663083+taskingaijc@users.noreply.github.com>
2024-05-27 21:48:16 +0800

    test: add test for record and chunk

--- backend/run_web_test.sh
+++ backend/run_web_test.sh
@@ -4,5 +4,5 @@ set -e
 parent_dir="$(dirname "$(pwd)")"
 export PYTHONPATH="${PYTHONPATH}:${parent_dir}"
 echo "Starting tests..."
-pytest   ./tests/services_tests  -m "web_test or api_test"
+pytest   ./tests/services_tests  -m "web_test or api_test"  -q  --tb=no  --reruns 3 --reruns-delay 2
 echo "Tests completed."
