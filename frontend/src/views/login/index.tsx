34e8877c5a3029aa88734b15aecd9847363df7ba
Theodore McCary <theodore@kouhousha.co.jp>
2024-06-03 16:16:47 +0900

    add/edit: views/login/index i18n
    created locale and i18n calls for login page. - Theodore McCary

--- frontend/src/views/login/index.tsx
+++ frontend/src/views/login/index.tsx
@@ -7,8 +7,10 @@ import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import style from './login.module.scss'
 import { fetchLogin } from '@/axios/index'
+import { useTranslation } from 'react-i18next';
 
 function Login() {
+    const { t } = useTranslation('views/login/index');
     const [loading, setLoading] = useState(false)
     const navigate = useNavigate()
     const onFinish = async (val: object) => {
@@ -17,7 +19,7 @@ function Login() {
             const res = await fetchLogin(val)
             localStorage.setItem('token', res.data.token)
             await navigate('/project/home')
-            toast.success('Signin successful')
+            toast.success(t('signinSuccessful'))
         } catch (error) {
             toast.error(error.response.data.error.message)
         }
@@ -30,33 +32,33 @@ function Login() {
                     <img src={aiIcon} alt="social-icon"></img>
                     <Beta style={{ marginLeft: '12px' }} />
                 </div>
-                <div className={style["welcome-font"]}>Welcome back</div>
+                <div className={style["welcome-font"]}>{t('welcomeBack')}</div>
                 <Form layout="vertical" onFinish={onFinish}>
                     <Form.Item
-                        label="Username"
+                        label={t('usernameLabel')}
                         name="username"
                         rules={[
                             {
                                 required: true,
-                                message: 'Please input your username.',
+                                message: t('usernameRequiredMessage'),
                             },
 
                         ]}
                     >
-                        <Input placeholder='Enter your username' className={style["input-edit"]} />
+                        <Input placeholder={t('usernamePlaceholder')} className={style["input-edit"]} />
                     </Form.Item>
 
                     <Form.Item
-                        label="Password"
+                        label={t('passwordLabel')}
                         name="password"
                         rules={[
                             {
                                 required: true,
-                                message: 'Please input your password.',
+                                message: t('passwordRequiredMessage'),
                             },
                         ]}
                     >
-                        <Input placeholder="Enter your password" type="password" className={style["input-edit"]} />
+                        <Input placeholder={t('passwordPlaceholder')} type="password" className={style["input-edit"]} />
                     </Form.Item>
                     <div className={style.remember}>
 
@@ -68,7 +70,7 @@ function Login() {
                             className={style["submit-btn"]}
                             loading={loading}
                         >
-                            Sign in
+                            {t('signinButton')}
                         </Button>
                     </Form.Item>
                 </Form>
