135f5a73bd367009dfdee50f828ff2b164a1df55
Theodore McCary <theodore@kouhousha.co.jp>
2024-06-05 13:51:28 +0900

    add: deleteModal i18n
    Added i18n. - Theodore McCary

--- frontend/src/components/deleteModal/index.tsx
+++ frontend/src/components/deleteModal/index.tsx
@@ -8,7 +8,7 @@ import closeIcon from '../../assets/img/x-close.svg'
 import { deleteProjectType } from '@/constant/index'
 import { useTranslation } from "react-i18next";
 const DeleteModal = (props: deleteProjectType) => {
-    const { t } = useTranslation();
+    const { t } = useTranslation(['common']);
     const [deleteLoading, setDeleteLoading] = useState(false)
     const { title, projectName, open, onDeleteConfirm, onDeleteCancel, describe, buttonType = 'delete-button' } = props
     const parts = describe.split(new RegExp(`(${projectName})`, 'i'))
@@ -25,10 +25,10 @@ const DeleteModal = (props: deleteProjectType) => {
     const customFooter = (
         <div>
             <Button key="back" onClick={handleCancel} className='cancel-button'>
-                {t('cancel')}
+                {t('cancel', {ns: 'common'})}
             </Button>
             <Button key="submit"  onClick={handleOk} loading={deleteLoading} className={`${buttonType} ${styles.button}`}>
-                {buttonType === 'delete-button' ? t('delete') : 'Confirm'}
+                {buttonType === 'delete-button' ? t('delete', {ns: 'common'}) : t('confirm', {ns: 'common'})}
             </Button>
         </div>
     );
