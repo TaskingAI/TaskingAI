20fb2520cbc5899bdddd8c4c6b0f95b5621eb9c0
Theodore McCary <theodore@kouhousha.co.jp>
2024-06-06 10:29:01 +0900

    add/edit: parameterTable i18n
    Added i18n. - Theodore McCary

--- frontend/src/components/parameterTable/index.tsx
+++ frontend/src/components/parameterTable/index.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import styles from './parameter.module.scss';
 import MessageSuccess from '../../assets/img/messageSuccess.svg?react'
+import { useTranslation } from 'react-i18next';
 
 const ParameterTable = ({ parameters }) => {
+  const { t } = useTranslation(['components/parameterTable/index', 'common'])
   return (
     <div className={styles.table}>
-      <div className={`${styles.cell} ${styles.header} ${styles.headerName}`}>Parameter name</div>
-      <div className={`${styles.cell} ${styles.header} ${styles.headerType}`}>Type</div>
-      <div className={`${styles.cell} ${styles.header} ${styles.headerRequired}`}>Required</div>
-      <div className={`${styles.cell} ${styles.header} ${styles.headerDescription}`}>Description</div>
+      <div className={`${styles.cell} ${styles.header} ${styles.headerName}`}>{t('parameterName')}</div>
+      <div className={`${styles.cell} ${styles.header} ${styles.headerType}`}>{t('type', {ns: 'common'})}</div>
+      <div className={`${styles.cell} ${styles.header} ${styles.headerRequired}`}>{t('required')}</div>
+      <div className={`${styles.cell} ${styles.header} ${styles.headerDescription}`}>{t('description', {ns: 'common'})}</div>
       {parameters.map((param, index) => (
         <React.Fragment key={index}>
           <div className={styles.cell}>{param.name}</div>
