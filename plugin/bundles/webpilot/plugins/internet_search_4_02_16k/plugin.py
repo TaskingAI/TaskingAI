2c9a4afec733db62c32d15e2840dd2048e46d46f
Link <wlk955@hotmail.com>
2024-07-04 16:27:28 +0800

    feat: add a new plugin webpilot

---
+++ plugin/bundles/webpilot/plugins/internet_search_4_02_16k/plugin.py
@@ -0,0 +1,31 @@
+import json
+
+from aiohttp import ClientSession
+
+
+from bundle_dependency import *
+from config import CONFIG
+
+
+class InternetSearch40216K(PluginHandler):
+    async def execute(self, credentials: BundleCredentials, plugin_input: PluginInput) -> PluginOutput:
+        content: str = plugin_input.input_params.get("content")
+        url: str = plugin_input.input_params.get("url", None)
+        webpilot_api_key: str = credentials.credentials.get("WEBPILOT_API_KEY")
+
+        api_url = f"https://beta.webpilotai.com/api/v1/watt"
+
+        headers = {"Authorization": f"Bearer {webpilot_api_key}", "Content-Type": "application/json"}
+
+        if url:
+            contents = " ".join([content, url])
+        body = {"model": "wp-watt-4.02-16k", "content": contents}
+
+        async with ClientSession() as session:
+            async with session.post(url=api_url, headers=headers, json=body, proxy=CONFIG.PROXY) as response:
+                if response.status == 200:
+                    results = await response.json()
+                    return PluginOutput(data={"results": results.get("content", "")})
+                else:
+                    data = await response.json()
+                    raise_provider_api_error(json.dumps(data))
