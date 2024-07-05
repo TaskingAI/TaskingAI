173b027012114bee37d1157cb9de0f6ef44580b1
Link <wlk955@hotmail.com>
2024-07-05 14:59:53 +0800

    feat: add a new plugin firecrawl

---
+++ plugin/bundles/fire_crawl/plugins/scrape_web/plugin.py
@@ -0,0 +1,32 @@
+import json
+
+from aiohttp import ClientSession
+
+
+from bundle_dependency import *
+from config import CONFIG
+
+
+class ScrapeWeb(PluginHandler):
+    async def execute(self, credentials: BundleCredentials, plugin_input: PluginInput) -> PluginOutput:
+        url: str = plugin_input.input_params.get("url")
+        only_main_content: bool = plugin_input.input_params.get("only_main_content", False)
+        screenshot: bool = plugin_input.input_params.get("screenshot", False)
+
+        fire_crawl_api_key: str = credentials.credentials.get("FIRE_CRAWL_API_KEY")
+
+        api_url = f"https://api.firecrawl.dev/v0/scrape"
+
+        headers = {"Authorization": f"Bearer {fire_crawl_api_key}", "Content-Type": "application/json"}
+
+        payload = {"url": url, "pageOptions": {"onlyMainContent": only_main_content, "screenshot": screenshot}}
+
+        async with ClientSession() as session:
+            async with session.post(url=api_url, headers=headers, json=payload, proxy=CONFIG.PROXY) as response:
+                if response.status == 200:
+                    data = await response.json()
+                    results = data.get("data", "")
+                    return PluginOutput(data={"results": json.dumps(results)})
+                else:
+                    data = await response.json()
+                    raise_provider_api_error(json.dumps(data))
