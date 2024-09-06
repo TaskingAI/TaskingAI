8240f3e563be7fb42ed4ba5b3a3045b97c2c87d5
Link <wlk955@hotmail.com>
2024-09-06 18:37:56 +0800

    feat: add debug-tool-call-hallucinations

--- inference/providers/debug/chat_completion.py
+++ inference/providers/debug/chat_completion.py
@@ -1,4 +1,5 @@
 from app.models import ModelSchema
+import copy
 from provider_dependency.chat_completion import *
 from app.models.tokenizer import estimate_input_tokens, estimate_response_tokens
 from typing import List, Dict, Optional
@@ -12,6 +13,19 @@ def _build_debug_response(message: ChatCompletionMessage):
         if isinstance(message.content, List):
             return str([c.model_dump() for c in message.content])
     return message.content
+
+
+TOOL_CALL_HALLUCINATION_MESSAGE = ChatCompletionAssistantMessage(
+    content=None,
+    role=ChatCompletionRole.assistant,
+    function_calls=[
+        {
+            "id": "P3lffDFvUpOJW3PxfB8ecoqw",
+            "name": "make_scatter_plot",
+            "arguments": {"x_values": [1, 2], "y_values": [3, 4]},
+        }
+    ],
+)
 
 
 class DebugChatCompletionModel(BaseChatCompletionModel):
@@ -37,11 +51,14 @@ class DebugChatCompletionModel(BaseChatCompletionModel):
             [function.model_dump() for function in functions] if functions else None,
             function_call,
         )
-
-        finish_reason = ChatCompletionFinishReason.stop
-        message_content = _build_debug_response(messages[-1])
-        message_content = message_content[(len(message_content) // 2) :].strip()
-        message = ChatCompletionAssistantMessage(content=message_content)
+        if provider_model_id == "debug-tool-call-hallucinations":
+            finish_reason = ChatCompletionFinishReason.function_calls
+            message = copy.deepcopy(TOOL_CALL_HALLUCINATION_MESSAGE)
+        else:
+            finish_reason = ChatCompletionFinishReason.stop
+            message_content = _build_debug_response(messages[-1])
+            message_content = message_content[(len(message_content) // 2) :].strip()
+            message = ChatCompletionAssistantMessage(content=message_content)
         output_tokens = estimate_response_tokens(message.model_dump())
         response = ChatCompletion(
             finish_reason=finish_reason,
@@ -69,32 +86,38 @@ class DebugChatCompletionModel(BaseChatCompletionModel):
         if provider_model_id == "debug-error":
             raise_http_error(ErrorCode.PROVIDER_ERROR, "Debug error for test")
 
+        if provider_model_id == "debug-tool-call-hallucinations":
+            output_message = copy.deepcopy(TOOL_CALL_HALLUCINATION_MESSAGE)
+            finish_reason = ChatCompletionFinishReason.function_calls
+
+        else:
+            # Extract the last message
+            message_content = _build_debug_response(messages[-1])
+            message_content = message_content[(len(message_content) // 2) :].strip()
+            # Split the message content into words
+            words = message_content.split()
+
+            # Simulate the streaming response by yielding each word
+            for i, word in enumerate(words):
+                yield ChatCompletionChunk(
+                    created_timestamp=get_current_timestamp_int(),
+                    index=i,
+                    delta=word,
+                )
+            output_message = ChatCompletionAssistantMessage(content=message_content)
+            finish_reason = ChatCompletionFinishReason.stop
+
         input_tokens = estimate_input_tokens(
             [message.model_dump() for message in messages],
             [function.model_dump() for function in functions] if functions else None,
             function_call,
         )
-        # Extract the last message
-        message_content = _build_debug_response(messages[-1])
-        message_content = message_content[(len(message_content) // 2) :].strip()
-        # Split the message content into words
-        words = message_content.split()
-
-        # Simulate the streaming response by yielding each word
-        for i, word in enumerate(words):
-            yield ChatCompletionChunk(
-                created_timestamp=get_current_timestamp_int(),
-                index=i,
-                delta=word,
-            )
-        message = ChatCompletionAssistantMessage(content=message_content)
-        output_tokens = estimate_response_tokens(message.model_dump())
-        finish_reason = ChatCompletionFinishReason.stop
+        output_tokens = estimate_response_tokens(output_message.model_dump())
         usage = ChatCompletionUsage(input_tokens=input_tokens, output_tokens=output_tokens)
         response = ChatCompletion(
             created_timestamp=get_current_timestamp_int(),
             finish_reason=finish_reason,
-            message=message,
+            message=output_message,
             usage=usage,
         )
         yield response
