9f6fa975418f07e41c2b9b3cf915ef5a98c07319
Link <wlk955@hotmail.com>
2024-10-29 16:15:15 +0800

    chore: update multiple plugins

--- plugin/bundles/fire_crawl/plugins/scrape_web/plugin.py
+++ plugin/bundles/fire_crawl/plugins/scrape_web/plugin.py
@@ -10,16 +10,25 @@ from config import CONFIG
 class ScrapeWeb(PluginHandler):
     async def execute(self, credentials: BundleCredentials, plugin_input: PluginInput) -> PluginOutput:
         url: str = plugin_input.input_params.get("url")
-        only_main_content: bool = plugin_input.input_params.get("only_main_content", False)
+        only_main_content: bool = plugin_input.input_params.get("only_main_content", True)
         screenshot: bool = plugin_input.input_params.get("screenshot", False)
 
         fire_crawl_api_key: str = credentials.credentials.get("FIRE_CRAWL_API_KEY")
 
-        api_url = f"https://api.firecrawl.dev/v0/scrape"
+        api_url = f"https://api.firecrawl.dev/v1/scrape"
 
         headers = {"Authorization": f"Bearer {fire_crawl_api_key}", "Content-Type": "application/json"}
 
-        payload = {"url": url, "pageOptions": {"onlyMainContent": only_main_content, "screenshot": screenshot}}
+        payload = {
+            "url": url,
+            "onlyMainContent": only_main_content,
+            "formats": [
+                "markdown",
+            ],
+        }
+
+        if screenshot:
+            payload.get("formats").append("screenshot@fullPage")
 
         async with ClientSession() as session:
             async with session.post(url=api_url, headers=headers, json=payload, proxy=CONFIG.PROXY) as response:
